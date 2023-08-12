3e24fba62a15a37e6e79dfa6c19c4f9ff35a62e6
Renan Martins <renanmartins1999@hotmail.com>
2023-08-12 18:37:02 -0300

    feat: Adding connection string to configuration file

--- az204-blob/Program.cs
+++ az204-blob/Program.cs
@@ -1,18 +1,20 @@
 ﻿using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
 
 Console.WriteLine("Azure Blob Storage exercise\n");
 
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
 // Run the examples asynchronously, wait for the results before proceeding
-ProcessAsync().GetAwaiter().GetResult();
+ProcessAsync(configuration).GetAwaiter().GetResult();
 
 Console.WriteLine("Press enter to exit the sample application.");
 Console.ReadLine();
 
-static async Task ProcessAsync()
+static async Task ProcessAsync(IConfigurationRoot configuration)
 {
     // Copy the connection string from the portal in the variable below.
-    string storageConnectionString = "CONNECTION STRING";
+    string storageConnectionString = configuration.GetConnectionString("BlobStorageCS");
 
     // Create a client that can authenticate with a connection string
     BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
