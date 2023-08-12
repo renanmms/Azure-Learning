cc230a5198fc0f9d117b7e7cd9f4b87b0530ef6c
Renan Martins <renanmartins1999@hotmail.com>
2023-08-12 18:42:30 -0300

    chore: updating configuration file

--- az204-blob/Program.cs
+++ az204-blob/Program.cs
@@ -14,11 +14,20 @@ Console.ReadLine();
 static async Task ProcessAsync(IConfigurationRoot configuration)
 {
     // Copy the connection string from the portal in the variable below.
-    string storageConnectionString = configuration.GetConnectionString("BlobStorageCS");
+    string storageConnectionString = configuration.GetConnectionString("StorageAccountCS");
 
     // Create a client that can authenticate with a connection string
     BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
 
-    // COPY EXAMPLE CODE BELOW HERE
+    //Create a unique name for the container
+    string containerName = "wtblob" + Guid.NewGuid().ToString();
+
+    // Create the container and return a container client object
+    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+    Console.WriteLine("A container named '" + containerName + "' has been created. " +
+        "\nTake a minute and verify in the portal." + 
+        "\nNext a file will be created and uploaded to the container.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
 
 }
