81cfbaf9fa2c041144efe9a95074b03c346bb0ab
Renan Martins <renanmartins1999@hotmail.com>
2023-08-15 05:48:30 -0300

    feat: Uploading text file to existing container

--- az204-blob/Program.cs
+++ az204-blob/Program.cs
@@ -15,19 +15,46 @@ static async Task ProcessAsync(IConfigurationRoot configuration)
 {
     // Copy the connection string from the portal in the variable below.
     string storageConnectionString = configuration.GetConnectionString("StorageAccountCS");
+    string blobContainerName = configuration.GetSection("BlobContainerName").Value;
 
     // Create a client that can authenticate with a connection string
     BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
 
-    //Create a unique name for the container
-    string containerName = "wtblob" + Guid.NewGuid().ToString();
-
-    // Create the container and return a container client object
-    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
-    Console.WriteLine("A container named '" + containerName + "' has been created. " +
-        "\nTake a minute and verify in the portal." + 
+    // Get the container and return a container client object
+    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+    Console.WriteLine("A container named '" + containerClient.Name + "' has been founded. " +
         "\nNext a file will be created and uploaded to the container.");
+    
+    await UploadTextFileToBlob(containerClient, "");
+    
     Console.WriteLine("Press 'Enter' to continue.");
     Console.ReadLine();
 
+}
+
+static async Task UploadTextFileToBlob(BlobContainerClient containerClient, string filePath){
+    // Create a local file in the ./data/ directory for uploading and downloading
+    string localPath = "./data/";
+    string fileName = "wtfile" + Guid.NewGuid().ToString() + ".txt";
+    string localFilePath = Path.Combine(localPath, fileName);
+
+    // Write text to the file
+    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+    // Get a reference to the blob
+    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+    Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+    // Open the file and upload its data
+    using (FileStream uploadFileStream = File.OpenRead(localFilePath))
+    {
+        await blobClient.UploadAsync(uploadFileStream);
+        uploadFileStream.Close();
+    }
+
+    Console.WriteLine("\nThe file was uploaded. We'll verify by listing" + 
+            " the blobs next.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
 }
